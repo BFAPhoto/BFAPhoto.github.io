0bea684fd66cd16e38c348e83427cdbb7e918699
BFAphoto <morales_gabe@yahoo.com>
2019-05-13 14:11:55 -0700

    test for code processing

---
+++ selfportrait/self_portrati.js
@@ -0,0 +1,20 @@
+function setup() {
+createCanvas(500, 500);
+}
+function draw() {
+background(252,198,8);
+fill(255,204,126)
+ellipse(250,255,150,200);
+fill (255)
+ellipse (275,235,20,10)
+ellipse (230,235,20,10)
+fill(255,204,126)
+ellipse(250,255, 10, 30);
+ellipse(253,280,30,10)
+
+fill(8,8,8)
+ellipse (275,235,10,10)
+ellipse (230,235,10,10)
+rect(260,210,30,5)
+rect(210,210,30,5)
+}
